6205f375db6a617585d2ff9902787dc56e3845d2
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-08-04 19:19:35 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (20.75%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int f(int n, vector<int>& dp) {
         if (n == 1)
             return 1;
-        if (n == 2)
-            return 2;
+        if (n == 0)
+            return 1;
         if (dp[n] != -1) {
             return dp[n];
         }
