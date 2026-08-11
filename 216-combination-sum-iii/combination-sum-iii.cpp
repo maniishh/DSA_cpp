c9f5013ac372f003a7737cd26bebb29b177fb866
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-08-11 15:24:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (41.79%) - LeetSync

--- 216-combination-sum-iii/combination-sum-iii.cpp
+++ 216-combination-sum-iii/combination-sum-iii.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void solve(int idx, int k, int target, vector<int>& ans) {
-        if (k == 0 && target == 0) {
+    vector<int> ans;
+    void solve(int idx, int k, int n) {
+        if (k == 0 && n == 0) {
             res.push_back(ans);
             return;
         }
-        if (k == 0 || target < 0) {
+        if (k == 0 || n < 0) {
             return;
         }
         for (int i = idx; i <= 9; i++) {
             ans.push_back(i);
-            solve(i + 1, k - 1, target - i, ans);
+            solve(i + 1, k - 1, n - i);
             ans.pop_back();
         }
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        vector<int> ans;
-        solve(1, k, n, ans);
+        solve(1, k, n);
         return res;
     }
 };
