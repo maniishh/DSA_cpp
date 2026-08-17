7936a9696d5c6ceff5de99560ea8e89fe1d0e170
Manish Kumar Yadav <162919290+maniishh@users.noreply.github.com>
2026-08-17 14:18:33 +0530

    Time: 34 ms (91.77%) | Memory: 52.8 MB (88.48%) - LeetSync

---
+++ 2809-create-hello-world-function/create-hello-world-function.js
@@ -0,0 +1,18 @@
+/**
+ * @return {Function}
+ */
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function () {
+    return function () {
+        return "Hello World";
+    };
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
+const f = createHelloWorld();
+f();
